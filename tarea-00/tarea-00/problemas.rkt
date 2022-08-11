085d1902ab63eaa68f1318ad5eee6f7f091c3817
DiegoTorresGon <die.torres.gonzalez@gmail.com>
2022-08-10 19:03:27 -0700

    fixed find-needle function

--- tarea-00/tarea-00/problemas.rkt
+++ tarea-00/tarea-00/problemas.rkt
@@ -9,37 +9,38 @@
 
 ;; 3.
 (define (circle-properties r)
-  (list (area-circle r) (* 2 pi r))
+  (list (area-circle r) (* 2 pi r)))
 
 ;; 4.
 (define (rectangle-properties rec)
-  (let ((a (car rec)) (b (cdr rec))))
-  (list (* a b) (+ (* 2 a) (* 2 b))))
+  (let ((a (car rec)) (b (cadr rec)))
+  (list (* a b) (+ (* 2 a) (* 2 b)))))
 
 ;; 5.
 (define (find-needle ls)
   (define (find-help ls ind)
     (cond 
-      ((null? ls) -1)
-      ((= (car ls) "needle") (+ ind 1))
-      (else (find-help ls (+ ind 1)))))
+      ((empty? ls) -1)
+      ((equal? (car ls) "needle") (+ ind 1))
+      (else (find-help (rest ls) (+ ind 1)))))
   (find-help ls -1))
 
+(if (condicion) valor_verdadero valor_negativo)
 ;; 6.
 (define (abs x)
-  ...)
+  (if (< x 0) (- x) x))
 
 ;; 7.
-(define (inclis1 ls)
-  ...)
+;(define (inclis1 ls)
+;  ...)
 
 ;; 8.
-(define (even? x)
-  ...)
+;(define (even? x)
+;  ...)
 
 ;; 9.
-(define another-add
-  (lambda (n m)
-    ...))
+;(define another-add
+;  (lambda (n m)
+;    ...))
 
 (provide (all-defined-out))
