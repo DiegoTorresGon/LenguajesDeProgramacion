0011e65342e8f914739ea157c4f15abde11d44b4
DiegoTorresGon <die.torres.gonzalez@gmail.com>
2022-08-11 16:56:37 -0700

    first two from tarea1

--- tarea-01/problemas.rkt
+++ tarea-01/problemas.rkt
@@ -1,5 +1,16 @@
 #lang racket
-
+ 
 ;; Escribe aquí tus soluciones
 
+(define (countdown n)
+    (if (= n -1)
+	'()
+	(cons n (countdown (- n 1)))))
+
+(define (insertL x y ls)
+  (cond 
+    ((empty? ls) '())
+    ((eqv? (car ls) x) (cons y (cons x (insertL x y (cdr ls)))))
+    (else (cons (car ls) (insertL x y (cdr ls))))))
+	
 (provide (all-defined-out))
