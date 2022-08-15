0acb32a927eed8ca52b698c7009f363c017ed352
Diego Torres <die.torres.gonzalez@gmail.com>
2022-08-15 12:50:49 -0700

    Solves until problem 14 - tarea01

--- tarea-01/problemas.rkt
+++ tarea-01/problemas.rkt
@@ -58,5 +58,50 @@
 	(append-help (cdr rev-a) (cons (car rev-a) b))))
   (append-help (reverse a) b))
  
+(define (repeat ls n)
+  (define (repeat-help ls n res)
+    (if (= n 0)
+	res
+	(repeat-help ls (- n 1) (append res ls))))
+  (repeat-help ls n '()))
+
+(define (same-lists* a b)
+  (cond
+    ((and (null? a) (null? b)) #t)
+    ((or (and (null? a) (not (null? b))) (and (null? b) (not (null? a)))) #f)
+    ((and (pair? (car a)) (pair? (car b))) 
+     (and (same-lists* (car a) (car b)) (same-lists* (cdr a) (cdr b))))
+    ((and (not (pair? (car a))) (not (pair? (car b))) (equal? (car a) (car b))) 
+     (same-lists* (cdr a) (cdr b)))
+    (else #f)))
+
+;Problema 12
+;((w x) y (z)) := ((w. (x . ())) . (y . ((z . ()) . ()))) 
+
+(define (binary->natural n_rev)
+  (define (binary-help n_rev start_place value)
+    (cond
+      ((empty? n_rev) value)
+      ((equal? 1 (car n_rev)) (binary-help (cdr n_rev) (* 2 start_place) (+ value start_place)))
+      (else (binary-help (cdr n_rev) (* 2 start_place) value))))
+  (binary-help n_rev 1 0))
+
+(define (div a b)
+  (define (div-help a b res)
+    (cond
+      ((= b 0) (error "div entre cero"))
+      ((< a 0) (error "algo salió mal"))
+      ((= a 0) res)
+      (else (div-help (- a b) b (+ res 1)))))
+  (cond
+    ((and (< a 0) (< b 0)) (div-help (- a) (- b) 0))
+    ((< a 0) (- (div-help (- a) b 0)))
+    ((< b 0) (- (div-help a (- b) 0)))))
+
+(define (append-map proc ls)
+  (
+
+  	
+
 
 (provide (all-defined-out))
